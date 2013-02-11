b75aba231b90e119061a9a694404665480272f2d
Aaron McGuire <aaron.mcguire@evb.com>
2013-02-10 18:33:16 -0800

    updated copy

--- www/lib/php/view/index.php
+++ www/lib/php/view/index.php
@@ -1,7 +1,7 @@
 <?php include DIR_TMPL.'/header.php'; ?>
 
 
-	<p class="lead">We all have liked things in the past... maybe too many things to remember. Now those very things are being used to advertise to your friends! I created this tool in efforts to reducing wall clutter that we might not have known we had. Click the button bellow to login, view the items that might be un-worthy of your 'liking' and then simply 'un-like' them.</p>
+	<p class="lead">We all have liked things in the past... maybe too many things to remember. Now those very things are being used to advertise to your friends! I created this tool in efforts to reduce wall clutter that we might not have known we had. Click the button bellow to login, view the items that might be un-worthy of your 'liking' and then simply 'un-like' them.</p>
 	<a class="cleanme btn btn-large btn-success" href="#" data-loading-text="loading..." data-complete-text="finished!">Cleanse Me Now</a>
 	<div class="alert alert-block alert-error">
 	    <button type="button" class="close" data-dismiss="alert">×</button>
